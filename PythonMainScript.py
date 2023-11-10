45cc34b00166a57948157119ea3c8740bb0b0fea
MrBladez <austin.minute@gmail.com>
2023-11-10 10:16:22 -0600

    Added Percents and Total amount of tails and heads

--- PythonMainScript.py
+++ PythonMainScript.py
@@ -2,7 +2,7 @@ import random
 resultHeads = 0
 resultTails = 0
 def findFlipResult():
-    randomResult  = random.randrange(1,2)
+    randomResult  = random.randrange(1,3)
     if randomResult == 1:
         flipResult = "Heads"
     elif randomResult == 2:
@@ -11,9 +11,14 @@ def findFlipResult():
 numberOfFlips = int(input("How many flips of a coin do you want to do? "))
 for i in range(numberOfFlips):
     flipResult = findFlipResult()
-    print("Flip number " + str(i + 1) + " was: " + flipResult)
     if flipResult == "Tails":
         resultTails += 1
     elif flipResult == "Heads":
         resultHeads += 1
+tailsPercent = round(resultTails / numberOfFlips, 3)
+headsPercent = round(resultHeads / numberOfFlips, 3)
+print("Number of tails flips was: " + str(resultTails))
+print("Number of heads flips was: " + str(resultHeads))
+print("Percent of tails flips was: " + str(tailsPercent) + " and percent of heads was: " + str(headsPercent) + " .")
+
     
